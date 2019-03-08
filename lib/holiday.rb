98d093355d96453fac68302adb14dec207449baf
John Cho <johnycho16@gmail.com>
2019-03-08 22:35:46 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -78,6 +78,7 @@ def all_supplies_in_holidays(holiday_hash)
       if holiday.to_s.include? "_"
         # array = holiday.to_s.split("_")
         # array.collect do |word|
+        holiday.to_s.split("_").collect do |word|
           word.capitalize!
         end
         puts array.inspect
