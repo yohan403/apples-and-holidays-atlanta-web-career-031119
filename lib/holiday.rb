16213b6231b74e6aa32d2e1fb1d86af95cd62b78
John Cho <johnycho16@gmail.com>
2019-03-08 22:51:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -95,8 +95,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season,holiday|
     holiday.each do |holiday,supply|
       if supply.include?("BBQ")
-        array = []
-        array.push(holiday)
+        holiday
       end
       return array
     end
