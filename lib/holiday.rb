0f286744a0ba05635ef77983f5fdfd6704999200
John Cho <johnycho16@gmail.com>
2019-03-08 22:44:27 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -93,7 +93,7 @@ def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
   holiday_hash.each do |season,holiday|
-    puts holiday.values.inspect
+    puts holiday.values.flatten.inspect
   end
 end
 
